7cb1848649f9462e3f4209996d941e26c262c0d8
askennladd <115483536+githubbyuserngoc@users.noreply.github.com>
2023-05-08 14:46:51 +0700

    Add files via upload

---
+++ map.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys 
+import string
+ 
+for line in sys.stdin: 
+    line = line.strip() 
+    words = line.split() 
+    for w in words: 
+        table = w.maketrans('', '', string.punctuation)
+        w = w.translate(table).lower() 
+        print(w, '\t', 1)
